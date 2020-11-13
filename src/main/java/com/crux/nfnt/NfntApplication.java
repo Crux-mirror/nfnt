c62568d380ad0e70d13a8d2e442d0d94eb92416d
shanlin.tu <shanlin.tu@cisdi.com.cn>
2020-11-13 10:33:36 +0800

    nfnt-init

---
+++ src/main/java/com/crux/nfnt/NfntApplication.java
@@ -0,0 +1,13 @@
+package com.crux.nfnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crux/nfnt/NfntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crux.nfnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
